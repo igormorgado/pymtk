fb61d0439d9b0725aa9e024cbbc33be735b8d259
Igor Morgado <morgado.igor@gmail.com>
2018-10-15 12:29:35 -0300

    Added flipall

--- pymtk/__init__.py
+++ pymtk/__init__.py
@@ -3,6 +3,8 @@
 """Top-level package for pymtk."""
 
 from sympy import nsimplify, Matrix
+from numpy import s_
+
 from pymtk.pymtk import Gradient, Divergent
 
 __author__ = """Igor Morgado"""
@@ -21,3 +23,7 @@ def rationalize(x, tolerance=10e-9):
     rationalized = [nsimplify(e, tolerance=tolerance) for e in Mx]
     return Matrix(rationalized).reshape(*shape)
 
+def flipall(a):
+    """Flip all axes of a given matrix (sparse or not)"""
+    sl = np.s_[::-1] 
+    return a[tuple([sl]*a.ndim)]
